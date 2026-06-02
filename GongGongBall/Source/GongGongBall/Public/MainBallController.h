6877a3569a0441b5442682fdbf87379ba4677684
WuYihui <qq2568520309@gmail.com>
2026-06-03 00:06:38 +0800

    Init

---
+++ GongGongBall/Source/GongGongBall/Public/MainBallController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MainBallController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GONGGONGBALL_API AMainBallController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
